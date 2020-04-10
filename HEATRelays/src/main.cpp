402f7cfa934d538b24db7173c40383f6977186b8
David James McCorrie <djmccorrie@gmail.com>
2020-04-10 15:09:32 +0100

    Arduino prototypes

---
+++ HEATRelays/src/main.cpp
@@ -0,0 +1,77 @@
+#include <Arduino.h>
+
+#include <CAN.h>
+#include <SPI.h>
+#include <math.h>
+
+const long mcp2515_can_freq = 500E3;
+const long mcp2515_spi_freq = 10E6;
+const long mcp2515_clock_freq = 16E6;
+
+uint8_t rxbuf[8] = {0};
+
+struct ThermData {
+  uint16_t therm1;
+  uint16_t therm2;
+  uint16_t therm3;
+};
+
+
+void packet_0x01_therm(uint8_t *msg){
+  struct ThermData *data = (struct ThermData *) msg;
+
+  float therm1 = ((float) data->therm1) / 100;
+  float therm2 = ((float) data->therm2) / 100;
+  float therm3 = ((float) data->therm3) / 100;
+
+  Serial.print(" T1[");
+  Serial.print(therm1);
+  Serial.print("] T2[");
+  Serial.print(therm2);
+  Serial.print("] T3[");
+  Serial.print(therm3);
+  Serial.println("] C");
+}
+
+
+void onReceive(int packetSize) {
+  int byte_no = 0;
+
+  Serial.print("packet with id 0x");
+  Serial.println(CAN.packetId(), HEX);
+
+  while (CAN.available()) {
+    rxbuf[byte_no++] = (uint8_t) CAN.read();
+  }
+
+  switch (CAN.packetId()) {
+    case 0x01:
+      packet_0x01_therm(rxbuf);
+      break;
+  }
+
+}
+
+
+void setup() {
+  Serial.begin(115200);
+
+  //CAN.setPins(can_cs, can_irq);
+  CAN.setSPIFrequency(mcp2515_spi_freq);
+  CAN.setClockFrequency(mcp2515_clock_freq);
+
+  while (!CAN.begin(mcp2515_can_freq)) {
+    Serial.println("Starting CAN failed!");
+
+    delay(500);
+  }
+
+  // register the receive callback
+  CAN.onReceive(onReceive);
+
+  Serial.println("CAN Initialised");
+}
+
+void loop() {
+
+}
---
+++ HEATThermistorNode/src/main.cpp
@@ -0,0 +1,115 @@
+#include <Arduino.h>
+
+#include <CAN.h>
+#include <SPI.h>
+#include <math.h>
+
+const int fixed_res = 6800;
+const int therm_res = 10000;
+const int therm_b = 3950;
+const int adc_max = 1023;
+
+const int thermPin1 = A0;
+const int thermPin2 = A1;
+const int thermPin3 = A2;
+
+const int thermEnablePin1 = 7;
+const int thermEnablePin2 = 6;
+const int thermEnablePin3 = 5;
+
+const int can_cs = 10;
+const int can_irq = 2;
+const int can_packet_size = 6;
+
+struct ThermData {
+  uint16_t therm1;
+  uint16_t therm2;
+  uint16_t therm3;
+};
+
+const long mcp2515_can_freq = 500E3;
+const long mcp2515_spi_freq = 10E6;
+const long mcp2515_clock_freq = 16E6;
+
+
+float get_therm_value(int pin, int enable_pin) {
+  float value = 0;
+
+  digitalWrite(enable_pin, HIGH);
+  value = analogRead(pin);
+  digitalWrite(enable_pin, LOW);
+
+  value = fixed_res * (value / (adc_max - value));
+
+  value = log(value / therm_res) / therm_b;
+  value += 1 / (25 + 273.15);
+  value = 1 / value;
+  value -= 273.15;
+
+  return value;
+}
+
+void transmit_can_packet(float therm1, float therm2, float therm3) {
+  //CAN.wakeup();
+  uint8_t * buff;
+  struct ThermData data;
+
+  data.therm1 = (int) (therm1 * 100);
+  data.therm2 = (int) (therm2 * 100);
+  data.therm3 = (int) (therm3 * 100);
+
+  buff = (uint8_t *) &data;
+  
+  CAN.beginPacket(0x01);
+  CAN.write(buff[0]);
+  CAN.write(buff[1]);
+  CAN.write(buff[2]);
+  CAN.write(buff[3]);
+  CAN.write(buff[4]);
+  CAN.write(buff[5]);
+  CAN.endPacket();
+
+  //CAN.sleep();
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(thermEnablePin1, OUTPUT);
+  pinMode(thermEnablePin2, OUTPUT);
+  pinMode(thermEnablePin3, OUTPUT);
+
+  //CAN.setPins(can_cs, can_irq);
+  //CAN.setSPIFrequency(mcp2515_spi_freq);
+  //CAN.setClockFrequency(mcp2515_clock_freq);
+
+  while (!CAN.begin(mcp2515_can_freq)) {
+    Serial.println("Starting CAN failed!");
+
+    delay(500);
+  }
+  
+  Serial.println("CAN Initialised");
+}
+
+void loop() {
+  float therm1 = get_therm_value(thermPin1, thermEnablePin1);
+  float therm2 = get_therm_value(thermPin2, thermEnablePin2);
+  float therm3 = get_therm_value(thermPin3, thermEnablePin3);
+
+  Serial.print("therm1 = ");
+  Serial.print(therm1);
+  Serial.print("|");
+  Serial.print((int) (therm1 * 100));
+  Serial.print("\t therm2 = ");
+  Serial.print(therm2);
+  Serial.print("|");
+  Serial.print((int) (therm2 * 100));
+  Serial.print("\t therm3 = ");
+  Serial.print(therm3);;
+  Serial.print("|");
+  Serial.println((int) (therm3 * 100));
+
+  transmit_can_packet(therm1, therm2, therm3);
+  delay(500);
+}
